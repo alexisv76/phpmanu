5ac369a250a96dbee564e7a6f546e440d8d9f2de
alexis <vauchel.alexis@hotmail.fr>
2021-01-14 14:22:11 +0100

    exo5

---
+++ partie2/exo5.php
@@ -0,0 +1,12 @@
+   <?php
+     
+      $gender = 'Homme';
+      if ($gender != 'Homme' )
+      {
+          echo "C'est une développeuse !!! ";
+      }
+      else 
+      {
+        echo "C'est un développeur !!! " ;
+      }
+    ?>
